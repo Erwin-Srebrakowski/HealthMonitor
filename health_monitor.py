31fc76e92e47e6a2756ff2dc3f29d7547afb9038
Erwin-Srebrakowski <esrebrakowski@gmail.com>
2021-10-14 19:15:01 +0100

    finshed task

--- health_monitor.py
+++ health_monitor.py
@@ -7,13 +7,15 @@
 """
 HEALTH MONITOR
 
-the health monitor will ask you to input thse itemes
+the health monitor will ask you to input these itemes
 input: heart rate(hr), blood presure(bp), body temperature(bt), breathing status(bs). 
 return: the helth monitor will tell you if your healthy by checking if youre in the healthy status ie not too high not too low
 return: if youre input is not in  the health satus then it will tell you that youre not health
 """
 import json
 import numpy as np
+
+dataPATH = 'params.json'
 
 def take_measurement(min, max, metric):
   if metric == 'body temperature':
@@ -23,13 +25,11 @@ def take_measurement(min, max, metric):
 
   if (measurement < min) or (measurement > max):
     status = 'at risk'
-    
   else:
     status = 'healthy'
     
-  
   print(status)
-  print('________________________________________________________________________________________________')
+  print('__________________________________________________')
 
   return status
 
@@ -40,37 +40,21 @@ def read_data(file):
 
   return data
 
-def check_risk(data):
-  at_risk = []
+def check_risk(data, id):
+  at_risk = [id]
 
-  print("\nNew patient:")
+  print("\nChecking patient " + str(id))
 
-  # Heart Rate
-  hr_data = data['heart rate']  
-  if take_measurement(hr_data['min'], hr_data['max'], hr_data['metric']) == 'at risk':
-    at_risk.append(hr_data['metric'])
+  for measurement in data:
+    health_data = data[measurement]  
 
-  # Bloot Pressure
-  maxbp=1202
-  minbp=60
-  if take_measurement(minbp, maxbp, 'blood pressure') == 'at risk':
-    at_risk.append('blood pressure')
-
-  # Body Temprature
-  maxbt=37.2
-  minbt=36.1
-  take_measurement(minbt, maxbt, 'body temperature')
-
-  #Breathing Status
-  maxbs=20
-  minbs=12
-  take_measurement(minbs, maxbs, 'breathing status')
+    if take_measurement(health_data['min'], health_data['max'], health_data['metric']) == 'at risk':
+      at_risk.append(health_data['metric'])
 
   return at_risk
 
 
 def main():
-  dataPATH = 'params.json'
   data = read_data(dataPATH)
 
   nb_patients = int(input('Please specify number of patients: \n'))
@@ -88,21 +72,28 @@ def main():
       print('Please type y or n.')
 
   patients = []
+  patient_ids = np.zeros(nb_patients)
 
   for i in range (nb_patients):
-    patients.append(check_risk(data))
+    id = int(input('Please enter patient id: \n'))
+    patients.append(check_risk(data, id))
+    patient_ids[i] = id
+
+  ordered_ids = set(patient_ids) 
 
   if output:
     f = open('patient_data.txt', 'w')
 
     for patient in patients:
       for val in patient:
-        f.write(val + " ")
+        f.write(str(val) + " ")
       f.write("\n")
 
     f.close()
 
+  print('Checked patints ID:')
+  print(ordered_ids)
+
  
-  
 if __name__ == "__main__":
   main()
